90112fb80ee44a9d6d4a3d364f2f4a1ed60a8450
Yeji-Yoon <angelayoon0102@gmail.com>
2025-02-27 09:05:14 -0800

    made files

---
+++ src/components/CarList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CarCard from "./CarCard";
+
+function CarList({cars}){
+
+    return(
+        <main>
+            <ul classname="cards">
+                {cars.map((car)=> <CarCard car={car}/>)}
+            </ul>
+        </main>
+    )
+
+}
+
+export default CarCars
