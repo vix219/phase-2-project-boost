8f73bd892adf7c854a41738b645c7abf21d0d81d
VictoriaCarvalho <vix219@icloud.com>
2025-03-06 10:19:06 -0600

    ADD: Readme.md, fixed typos

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,13 +10,6 @@ const Home = () => {
 
     <div >
       <img className="logo" src={logo} ></img>
-      {/* <p className="logo-decor">----------</p> 
-      <h1 className="logo-text">BOOST MAX</h1>
-      <p className="logo-decor">----------</p>
-      <p className="logo-quote">
-        "Find the car that fits your drive, not just your driveway."
-      </p> */}
-      
 
       <CarList/>
      
